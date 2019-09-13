9042a790fd0f6c7ca22b0252678ec3e91bdf6b0f
elliotshero <elliot@sherodesigns.com>
2019-09-13 08:26:09 -0400

    Add Slick assets

--- wp-content/themes/oslo-child/front-page.php
+++ wp-content/themes/oslo-child/front-page.php
@@ -11,26 +11,26 @@
 	<meta name="description" content="Hudson Valley NY Architect specializing in residential Passive Houses that integrate refined design, comfort, durability and energy efficiency.">	
 	<title>Barry Price Architecture</title>
 <link rel='dns-prefetch' href='//fonts.googleapis.com' />
-	
-<link rel='stylesheet'  href='<?php echo $baseUrl; ?>/landing.css' type='text/css' media='all' />
 
 <link rel="icon" href="https://barryprice.com/wp-content/uploads/2018/03/ms-icon-310x310-150x150.png" sizes="32x32" />
 <link rel="icon" href="https://barryprice.com/wp-content/uploads/2018/03/ms-icon-310x310-300x300.png" sizes="192x192" />
 <link rel="apple-touch-icon-precomposed" href="https://barryprice.com/wp-content/uploads/2018/03/ms-icon-310x310-300x300.png" />
 <meta name="msapplication-TileImage" content="https://barryprice.com/wp-content/uploads/2018/03/ms-icon-310x310-300x300.png" />
 
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.css"/>
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.min.css"/>
+
+<link rel='stylesheet'  href='<?php echo get_stylesheet_directory_uri(); ?>/home.css' type='text/css' media='all' />
+
 </head>
 
 <body class="landing">
-	<div class="block-wrap">
-	<div class="one-block first"><a href="<?php echo $baseUrl; ?>/site/"><img class="initial" src="<?php echo $baseUrl; ?>/landing-images/Site_BW.jpg" alt="Site"><img class="hidden" src="<?php echo $baseUrl; ?>/landing-images/Site_Color.jpg"></a></div>
-	<div class="one-block second"><a href="<?php echo $baseUrl; ?>/shelter/"><img class="initial" src="<?php echo $baseUrl; ?>/landing-images/Shelter_BW.jpg" alt="Shelter"><img class="hidden" src="<?php echo $baseUrl; ?>/landing-images/Shelter_Color.jpg"></a></div>
-	<div class="one-block third"><a href="<?php echo $baseUrl; ?>/space/"><img class="initial" src="<?php echo $baseUrl; ?>/landing-images/Space_BW.jpg" alt="Space"><img class="hidden" src="<?php echo $baseUrl; ?>/landing-images/Space_Color.jpg"></a></div>
-	<div class="one-block fourth"><a href="<?php echo $baseUrl; ?>/moment/"><img class="initial" src="<?php echo $baseUrl; ?>/landing-images/Moment_BW.jpg" alt="Moment"><img class="hidden" src="<?php echo $baseUrl; ?>/landing-images/Moment_Color.jpg"></a></div>
-</div>
+	
+
 
 <script type='text/javascript' src='<?php echo $baseUrl; ?>/wp-includes/js/jquery/jquery.js?ver=1.12.4'></script>
 <script type='text/javascript' src='<?php echo $baseUrl; ?>/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1'></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js"></script>
 
-		</body>
+</body>
 </html>
